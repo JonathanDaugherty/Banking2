a60356c2722a06a199a4db600d0e46114fa64751
jmdaugherty94@gmail.com <jmdaugherty94@gmail>
2021-02-16 09:16:59 -0500

    Add project files.

---
+++ Banking2/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking2 {
+    class Account {
+        public string RoutingNumber { get; set; }
+        public string AccountNumber { get; set; }
+        public string Description { get; set; }
+        public string Accounttype { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount) {
+            if (amount > 0) 
+                { Balance += amount; }
+        }
+
+        public void withdrawal(decimal amount) {
+            if(amount < 0) {
+                return;
+            }
+            if(Balance >= amount) {
+                Balance -= amount;
+            } else {
+                Console.WriteLine("Insufficient Funds");
+            }
+        }
+            
+
+            
+        
+    }
+    
+}
---
+++ Banking2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Banking2 {
+    class Program {
+        static void Main(string[] args) {
+            var Acct2 = new Account();
+            Acct2.AccountNumber = "9682367";
+            Acct2.Description = "Checking";
+
+            Acct2.Deposit(500);
+            Console.WriteLine($"Current Balance in {Acct2.Description} is {Acct2.Balance}" );
+            Acct2.withdrawal(600);
+
+
+
+        }
+    }
+}
