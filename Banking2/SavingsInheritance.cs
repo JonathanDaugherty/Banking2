4bb8fe2569aee7afd65c56636d50d9891f9bb29e
jmdaugherty94@gmail.com <jmdaugherty94@gmail>
2021-02-17 22:26:51 -0500

    inheritance hw

---
+++ Banking2/SavingsInheritance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking2 {
+    class SavingsInheritance : Account {
+        
+        public decimal InterestRate { get; set; } = 0.12m;
+
+        private decimal Interest(int months) {
+            return Balance * (InterestRate / 12) * months;
+        }
+
+        public void PayInterest(int months) {
+            var interest = Interest(months);
+            Deposit(interest);
+        }
+       
+        public SavingsInheritance() { }
+
+    }
+}
